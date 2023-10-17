2371ef9e92976a1c9807bf03f147ea7ff4767057
Alexandra8900 <148012127+Alexandra8900@users.noreply.github.com>
2023-10-18 02:02:54 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
-import flask
-from flask import render_template
 import pickle
 import sklearn
-from sklearn.ensemble import GradientBoostingRegressor
-app = flask.Flask(__name__, template_folder= 'templates')
-@app.route('/', methods = ['POST', 'GET'])
-@app.route('/index', methods = ['POST', 'GET'])
+from sklearn.ensemble import RandomForestRegressor
+import flask
+from flask import render_template
 
-def main ():
-     if flask.request.method == 'GET':
-         return render_template ('main.html')
-     if flask.request.method == 'POST':
-         with open ('model_gbr1.pkl', 'rb') as f:
-             loaded_model = pickle.load (f)
-         with open ('model_gbr2.pkl', 'rb') as b:
-             loaded_model2 = pickle.load (b)
-         X1 = float (flask.request.form['IW'])
-         X2 = float (flask.request.form['IF'])
-         X3 = float (flask.request.form['VW'])
-         X4 = float (flask.request.form['FP'])
-         y1_gbr1 = loaded_model.predict([[X1,X2,X3,X4]])
-         y2_gbr2 = loaded_model2.predict([[X1,X2,X3,X4]])
+app = flask.Flask(__name__, template_folder='template')
 
-         return render_template('main.html', result = (y1_gbr1, y2_gbr2) )
+@app.route('/', methods=['POST', 'GET'])
+
+@app.route('/index', methods=['POST', 'GET'])
+def main():
+    if flask.request.method == 'GET':
+        return render_template('main.html')
+
+    if flask.request.method == 'POST':
+        with open('model_rfr1.pkl', 'rb') as f:
+            loaded_model1 = pickle.load(f)
+        with open('model_rfr2.pkl', 'rb') as b:
+            loaded_model2 = pickle.load(b)
+        x1 = float(flask.request.form['IW'])
+        x2 = float(flask.request.form['IF'])
+        x3 = float(flask.request.form['VW'])
+        x4 = float(flask.request.form['FP'])
+        y_rfr1 = loaded_model1.predict([[x1, x2, x3, x4]])
+        y_rfr2 = loaded_model2.predict([[x1, x2, x3, x4]])
+        return render_template('main.html', result=(y_rfr1, y_rfr2))
+
 
 if __name__ == '__main__':
-     app.run()
+    app.run()
